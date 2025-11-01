8496ffdaaaf64c9db206eb8c435cba2c93abfb8b
Houston Fortney <hfortney@path-robotics.com>
2025-11-01 18:34:35 -0400

    Add git config role

---
+++ roles/configure_git/tests/test_configure_git.py
@@ -0,0 +1,5 @@
+def test_git_config_spot_check(host):
+    user = host.user()
+    git_config_file = host.file(f"{user.home}/.gitconfig")
+    assert git_config_file.exists
+    assert git_config_file.contains("editor = vim")
