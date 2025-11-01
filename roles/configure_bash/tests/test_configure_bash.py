57f29263678835e08a432e9d3d46087102d5d36e
Houston Fortney <hfortney@path-robotics.com>
2025-11-01 14:06:33 -0400

    Fix ruff errors

--- roles/configure_bash/tests/test_configure_bash.py
+++ roles/configure_bash/tests/test_configure_bash.py
@@ -1,18 +1,18 @@
 def test_bashrc_local_file_created(host):
     user = host.user()
-    bashrc_local = host.file(f'/{user.home}/.bashrc_local')
+    bashrc_local = host.file(f"{user.home}/.bashrc_local")
     assert bashrc_local.exists
 
 
 def test_bashrc_local_is_sourced(host):
     command = host.run("bash -i -c 'echo $BASHRC_LOCAL_SOURCED'")
     assert command.rc == 0
-    assert command.stdout.strip() == 'true'
+    assert command.stdout.strip() == "true"
 
 
 def test_bash_aliases_file_created(host):
     user = host.user()
-    bash_aliases = host.file(f'/{user.home}/.bash_aliases')
+    bash_aliases = host.file(f"{user.home}/.bash_aliases")
     assert bash_aliases.exists
 
 
--- roles/install_utils/tests/test_install_utils.py
+++ roles/install_utils/tests/test_install_utils.py
@@ -8,7 +8,7 @@ def load_role_defaults():
 
 def test_packages_are_installed(host):
     packages = load_role_defaults().get("install_utils_packages", [])
-    assert packages, "Package list 'util_packages' should not be empty"
+    assert packages, "Package list should not be empty"
 
     for package_name in packages:
         package = host.package(package_name)
