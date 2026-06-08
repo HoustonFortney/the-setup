af771deeccf4aa5a48dd4bb2f4f5e8b852bff8bf
Houston Fortney <hfortney@path-robotics.com>
2026-06-08 00:13:40 -0400

    Let live grep look in .github/similar folders

--- roles/setup_neovim/files/config/init.lua
+++ roles/setup_neovim/files/config/init.lua
@@ -221,6 +221,11 @@ require("lazy").setup({
                     find_files = {
                         hidden = true,
                     },
+                    live_grep = {
+                        additional_args = function()
+                            return { "--hidden" }
+                        end,
+                    },
                 },
                 extensions = {
                     ["ui-select"] = {
